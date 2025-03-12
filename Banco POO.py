729e77fc852fa6578c61f0dbd5af12c58ff857ef
Kkraties13 <160873692+Kkraties13@users.noreply.github.com>
2025-03-11 21:35:44 -0300

    Tentativa nova
    Está ocorrendo um problema ao realizar a chamada da classe "Banco", presente no arquivo "class_Banco". Fazendo com que o programa rode inteiramente ao invés de somente a parte indicada
    
    resolver isso

---
+++ Banco POO.py
@@ -0,0 +1,14 @@
+# filepath: c:\Users\gabri\OneDrive\Documentos\VS Code  Pastas\Sistema de Banco\Banco POO.py
+
+from class_Banco import Banco
+
+# login_usuario = input("Digite o nome de usuário ou e-mail: ")
+# senha_usuario = input("Digite sua senha: ")
+
+# Exemplo de uso do sistema de banco
+if __name__ == "__main__":
+
+    # banco = Banco(login_usuario, senha_usuario)
+    
+    Banco.Login()   
+    
---
+++ class_Banco.py
@@ -0,0 +1,133 @@
+import os
+
+def enter():
+    
+    print("Pressione ENTER para continuar")
+    input()
+    
+    os.system("cls")
+    
+class Banco:
+    
+    def __init__(self, login, senha):
+        self.login = login.upper()
+        self.senha = senha.upper()
+        self.saldo_usuario = 0
+        self.extrato_usuario = ["Histórico de operações"]
+    
+
+# login = "Teste"
+# senha = "1234"
+
+# saldo_usuario = 0
+# extrato_usuario = ["Histórico de operações"]
+
+    def Login(self, login_usuario = input("Digite o nome de usuário ou e-mail: "), senha_usuario = input("Digite sua senha: ")):
+        
+        while login_usuario.upper() != self.login.upper() or senha_usuario != self.senha:
+            
+            print("Login ou senha inválidos. Tente novamente.")
+
+            enter()
+            
+            login_usuario = input("Digite o nome de usuário ou e-mail: ")
+            senha_usuario = input("Digite sua senha: ")
+
+        os.system("cls")
+
+        print("Você está logado")
+
+        enter()
+        
+        
+        
+    def Saque(self, valor_saque = float(input("Digite o valor para saque: "))):
+
+        os.system("cls")
+        
+        if self.saldo_usuario > 0:
+
+            print("Saldo insuficiente\nFaça ao menos um depósito")
+            enter()     
+            
+        else:  
+            
+            valor_saque = float(input("Digite o valor para saque: "))
+            
+            saldo_usuario -= valor_saque
+            
+            self.extrato_usuario.append(f"- {str(valor_saque)}")
+            
+            print(f"Saque realizado com sucesso\nO saldo atual é de {saldo_usuario} Reais")
+            
+            enter()
+            
+            
+            
+    def Deposito(self, valor_deposito = float(input("Digite o valor do depósito: "))):
+
+            os.system("cls")        
+                
+            valor_deposito = float(input("Digite o valor do depósito: "))
+
+            saldo_usuario += valor_deposito
+            
+            self.extrato_usuario.append(f"+ {str(valor_deposito)}")
+            
+            print(f"Depósito realizado com sucesso\nO saldo atual é de {saldo_usuario} Reais")
+            
+            enter()
+            
+            
+            
+    def menu(self, opcao = input("Funções do banco:\n\tDigite S para Saque\n\tDigite D para Depósito\n\tDigite SA para Exibir o Saldo\n\tDigite E para ver o extrato\n\tDigite SC para saír da conta\nDigite aqui: ")):
+
+        while opcao.upper() != "SC":
+            
+            if opcao.upper() == "S":
+                
+                self.Saque()
+            
+            elif opcao.upper() == "D":
+                
+                self.Deposito()
+            
+            elif opcao.upper() == "SA":
+
+                os.system("cls")
+                
+                print(f"O saldo atual da conta é: {self.saldo_usuario}")
+                
+                enter()
+                
+            elif opcao.upper() == "E":
+                
+                os.system("cls")
+                
+                for posicao_operacao in range(len(self.extrato_usuario)):
+                    
+                    print(self.extrato_usuario[posicao_operacao])
+            
+                
+                print(f"Fim do extrato\n\nSaldo da Conta: R${self.saldo_usuario}\n")
+                
+                enter()
+            
+            else:
+                
+                os.system("cls")
+                
+                print("A chave de operação não consta no sistema ou foi digitada errada")
+                
+                enter()
+                
+                
+                    
+                
+            
+            opcao = input("Funções do banco:\n\tDigite S para Saque\n\tDigite D para Depósito\n\tDigite SA para Exibir o Saldo\n\tDigite E para ver o extrato\n\tDigite SC para saír da conta\nDigite aqui: ")
+
+        
+        os.system("cls")
+        
+        print("O Sistema foi finalizado com sucesso!")
